cd00ace0dab13fcc09939e315852115d74f6b54d
roberto encalada <rocaenca@espol.edu.ec>
2024-01-08 10:44:43 -0500

    gestión de permisos, guía 22

--- routes/suppliers.js
+++ routes/suppliers.js
@@ -6,6 +6,13 @@ const Suppliers = require('../models').suppliers;
 
 /* GET suppliers listing. */
 router.get('/findAll', function(req, res, next) {
+    /* Verificador de autorización */
+
+    const { role } = req.user;
+
+    if (role !== process.env.ADMIN) {
+        return res.sendStatus(401);
+    }
     Suppliers.findAll({})
     .then(data => {
         res.json(data)
